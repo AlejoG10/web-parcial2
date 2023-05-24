e1ab5bf6d469546d620b92e197ff8152cb40a8fc
Alejandro <alejogonza99@hotmail.com>
2023-05-24 18:12:01 -0500

    Add initial structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { IntlProvider } from "react-intl";
+import * as Pages from "./pages";
+import localeEs from "./locales/es.json";
+import localeEn from "./locales/en.json";
 
 function App() {
+  const [locale, setLocale] = useState(navigator.language);
+  const [localeMsgs, setLocaleMsgs] = useState({});
+
+  useEffect(() => {
+    if (locale === "en") setLocaleMsgs(localeEn);
+    else setLocaleMsgs(localeEs);
+  }, [locale]);
+
   return (
-    <div className="App text-rose-500">
-      TAILWIND
-    </div>
+    <IntlProvider locale={locale} messages={localeMsgs}>
+      <Router>
+        <Routes>
+          <Route exact index element={<Pages.Home />} />
+        </Routes>
+      </Router>
+    </IntlProvider>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { FormattedMessage } from "react-intl";
+
+export default function Home() {
+  return (
+    <div>
+      <FormattedMessage id="main" />
+    </div>
+  );
+}
